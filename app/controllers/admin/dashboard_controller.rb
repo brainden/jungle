9512f38e586806e48aff0dece7d5be0c16cec0b0
Brandon <brandonkilic@gmail.com>
2019-08-23 19:11:50 +0000

    added dashboard for admin

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,9 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name =>  ENV["HTTP_USER"],
   :password => ENV["HTTP_PASSWORD"], except: [:index]
+
   def show
+    @product_count = Product.all
+    @category_count = Category.all
   end
 end
