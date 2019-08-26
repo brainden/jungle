322fd20b1c282c29029c8deac73049bf12b7702d
Brandon <brandonkilic@gmail.com>
2019-08-26 21:20:45 +0000

    added rspec

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,35 @@
-class CategoriesController < ApplicationController
+class Admin::CategoriesController < ApplicationController
+  # http_basic_authenticate_with name: ENV['admin_name'], password: ENV['admin_pw']
 
-  def show
-    @category = Category.find(params[:id])
-    @products = @category.products.order(created_at: :desc)
+  def index
+    @categories = Category.order(name: :asc).all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+      :name,
+      # :description,
+      # :category_id,
+      # :quantity,
+      # :image,
+      # :price
+    )
   end
 
 end
